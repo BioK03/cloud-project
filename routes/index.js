9462ff9d5bf54957e244039255de22ec448ab010
Vlad <vladmourier@hotmail.fr>
2016-11-23 15:08:05 +0100

    Ajout de docker et travis

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var shortid = require('shortid');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,7 +8,11 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/copy', function (req, res, next){
-  res.render('copy', {submitted_text: req.body.text})
+  var id = shortid.generate();
+  var submitted_text = req.body.text;
+  console.log(id);
+
+  res.render('copy', {submitted_text: submitted_text});
 });
 
 module.exports = router;
